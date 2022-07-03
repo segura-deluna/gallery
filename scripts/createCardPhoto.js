5f6ef9f867b987e8ec0c04d296cff07e0ed55e25
Segura <segura-deluna@mail.ru>
2022-07-03 18:02:49 +0500

    add render photo on page photo

--- scripts/createCardPhoto.js
+++ scripts/createCardPhoto.js
@@ -23,20 +23,21 @@ export const createCardPhoto = async (data) => {
     href: `page.html?photo=${data.id}`,
   });
 
-  const photo = await loadImg(data.urls.small, data.alt_description);
+  const photo = await loadImg(data.urls.small, data.description);
 
   const author = createElem('a', {
     className: 'card__author',
     href: data.user.links.html,
   });
 
-  const avatarAuthor = new Image();
-  avatarAuthor.className = 'author__photo';
-  avatarAuthor.src = data.user.profile_image.medium;
-  avatarAuthor.width = '32';
-  avatarAuthor.height = '32';
-  avatarAuthor.alt = data.user.bio;
-  avatarAuthor.title = data.user.username;
+  const avatarAuthor = createElem('img', {
+    className: 'author__photo',
+    src: data.user.profile_image.medium,
+    width: '32',
+    height: '32',
+    alt: data.user.bio,
+    title: data.user.username,
+  });
 
   author.append(avatarAuthor);
 
--- scripts/renderPhoto.js
+++ scripts/renderPhoto.js
@@ -1,26 +1,61 @@
-export const renderPhoto = (photoWrapper, photo) => {};
+import { createElem } from './createElem.js';
+
+export const renderPhoto = (photoWrapper, photo) => {
+  const img = createElem('img', {
+    className: 'photo__picture',
+    src: photo.urls.full,
+    alt: photo.description,
+    style: 'max-height: 80vh;',
+  });
+
+  const author = createElem('a', {
+    className: 'photo__author',
+    href: photo.user.links.html,
+  });
+
+  const avatarAuthor = createElem('img', {
+    src: photo.user.profile_image.medium,
+    alt: photo.user.bio,
+    title: photo.user.username,
+  });
+
+  const userName = createElem('span', {
+    textContent: photo.user.username,
+  });
+
+  const photoControl = createElem('div', {
+    className: 'photo__control',
+  });
+
+  const photoLike = createElem('button', {
+    className: 'photo__like',
+    id: photo.id,
+    textContent: photo.likes,
+  });
+
+  const photoDownload = createElem('a', {
+    className: 'photo__download',
+    download: 'true',
+    href: photo.links.download,
+    target: '_blank',
+  });
+
+  author.append(avatarAuthor, userName);
+  photoControl.append(photoLike, photoDownload);
+  photoWrapper.append(img, author, photoControl);
+};
 
 {
   /*
-	<img
-		class="photo__picture"
-		src="https://images.unsplash.com/photo-1654363137357-9d897b5a20d7?crop=entropy&amp;cs=tinysrgb&amp;fm=jpg&amp;ixid=MnwzMDE0MzF8MHwxfGFsbHx8fHx8fHx8fDE2NTQ1MjMzNjE&amp;ixlib=rb-1.2.1&amp;q=80"
-		alt="null"
-		style="max-height: 80vh;">
-	<a
-		class="photo__author"
-		href="https://unsplash.com/@supergios">
-		<img
-			src="https://images.unsplash.com/profile-1600184424687-de96bd61fa67image?ixlib=rb-1.2.1&amp;crop=faces&amp;fit=crop&amp;w=64&amp;h=64"
-			alt="Always looking for the next adventure to capture the next shot. Seize the day. Sony A7 IV . There are more images behind my Unplash, for commercial projects please contact me.  John 3:16" title="Jonny Gios">
-		<span>Jonny Gios</span>
-	</a>
+
+
+
 	<div class="photo__control">
 		<button id="JIqH1ps4eK8" class="photo__like">30</button>
 		<a
 			class="photo__download"
 			download="true"
-			href="https://unsplash.com/photos/JIqH1ps4eK8/download?ixid=MnwzMDE0MzF8MHwxfGFsbHx8fHx8fHx8fDE2NTQ1MjMzNjE"
+			href=""
 			target="_blank">
 		</a>
 	</div>
